c738d71f0e23b1422d0e80e69f3f3391fda9a1ab
Yolomep <wiitong0394703@gmail.com>
2024-12-03 19:21:15 -0500

    gradcam

--- notebooks/testmodel.py
+++ notebooks/testmodel.py
@@ -10,61 +10,64 @@ import matplotlib.pyplot as plt
 model = tf.keras.models.load_model('cnn.keras')
 
 DATASET_PATH = "../Data/"
+IMAGE_PATH = "/Moderate Dementia/OAS1_0308_MR1_mpr-1_102.jpg"
 BATCH_SIZE = 32
 VALIDATION_SPLIT = 0.3
 IMAGE_SIZE = (248, 496)
 data_dir = pathlib.Path(DATASET_PATH)
 classes = ["Mild Dementia", "Moderate Dementia", "Non Demented", "Very mild Dementia"]
 
-train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
-        labels="inferred",
-        directory=data_dir,
-        batch_size=BATCH_SIZE,
-        validation_split=VALIDATION_SPLIT,
-        seed=0,
-        image_size=IMAGE_SIZE,
-        subset='both',
-        color_mode="grayscale")
+def compute_gradcam(model, image_array, last_conv_layer_name, class_idx=None):
 
-for images, labels in train_ds.take(1):
-    image = images[0]
+    image_tensor = tf.convert_to_tensor(image_array, dtype=tf.float32)
     
-    print(model.predict(image))
-    break
-    
-    layer_name = 'conv2d_2'  
-    last_conv_layer = model.get_layer(layer_name)
-    
-    grad_model = Model(
-        inputs=model.inputs,
-        outputs=[last_conv_layer.output, model.output]
+    last_conv_layer = model.get_layer(last_conv_layer_name)
+
+    grad_model = tf.keras.models.Model(
+        [model.inputs], 
+        [last_conv_layer.output, model.output]
     )
-    
+
     with tf.GradientTape() as tape:
-        conv_outputs, predictions = grad_model(images)
-        class_idx = np.argmax(predictions[0])  # Index of the predicted class
-        loss = predictions[:, class_idx]
+        tape.watch(image_tensor)
+        conv_outputs, predictions = grad_model(image_tensor)
+        if class_idx is None:
+            class_idx = tf.argmax(predictions[0])
+        class_output = predictions[:, class_idx]
 
-        # Compute gradients of the class score w.r.t. the feature map
-        grads = tape.gradient(loss, conv_outputs)
+    grads = tape.gradient(class_output, conv_outputs)
 
-        # Compute the mean intensity of the gradients over the feature map
-        pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    weights = tf.reduce_mean(grads, axis=(0, 1, 2))
 
-        # Multiply each channel by "how important it is" with respect to the predicted class
-        conv_outputs = conv_outputs[0]  # Remove batch dimension
-        heatmap = tf.reduce_mean(tf.multiply(pooled_grads, conv_outputs), axis=-1)
-        
-        heatmap = np.maximum(heatmap, 0)
-        heatmap /= np.max(heatmap)  # Normalize to [0, 1]
+    gradcam = tf.reduce_sum(weights * conv_outputs[0], axis=-1)
+    gradcam = tf.maximum(gradcam, 0)  # Apply ReLU
+    gradcam /= tf.reduce_max(gradcam)  # Normalize to [0, 1]
 
-        # Convert heatmap to RGB
-        heatmap = np.uint8(255 * heatmap)
+    return gradcam.numpy()
 
-        # Apply the heatmap to the input image
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        superimposed_img = cv2.addWeighted(images[0].astype('uint8'), 0.6, heatmap, 0.4, 0)
-        
-        plt.imshow(superimposed_img)
-        plt.axis('off')
-        plt.show()
+
+
+img = tf.keras.utils.load_img(DATASET_PATH + IMAGE_PATH, target_size=IMAGE_SIZE, color_mode="grayscale")
+image = tf.keras.preprocessing.image.img_to_array(img)
+image = np.expand_dims(image, axis=0)
+# print(model.predict(image))
+
+layer_name = 'conv2d_2'  
+
+heatmap = compute_gradcam(model, image, layer_name)
+
+image_original = np.array(image) / 255.0  
+
+# Plot the results
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 3, 1)
+plt.title("Original Image")
+plt.imshow(image_original.squeeze(), cmap='gray')  
+plt.axis("off")
+
+plt.subplot(1, 3, 2)
+plt.title("Grad-CAM Heatmap")
+plt.imshow(heatmap, cmap='viridis')
+plt.axis("off")
+
+plt.show()
